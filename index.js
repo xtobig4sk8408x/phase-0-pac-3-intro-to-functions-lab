f123593242959ac2f350ee634bb3b6793e316ed1
Theodore John Josef <john.josef@aol.com>
2022-09-12 18:34:45 -0700

    commit tree 8ddac693d1e0fcff214c085562b68e7e8e1deb31

---
+++ index.js
@@ -0,0 +1,41 @@
+function shout(string)
+{
+    return string.toUpperCase();
+}
+
+function whisper(string)
+{
+    return string.toLowerCase();
+}
+
+function logShout(string)
+{
+    console.log(string.toUpperCase());
+}
+
+function logWhisper(string)
+{
+    console.log(string.toLowerCase());
+}
+
+
+function sayHiToHeadphonedRoommate(string)
+{
+    let miHoyMeNoy
+switch (string)
+{
+
+
+    case string.toLowerCase():
+        console.log(string);
+        miHoyMeNoy = "I can't hear you!";
+        break;
+    case string.toUpperCase():
+        miHoyMeNoy = "YES INDEED!";
+        break;
+    case "Let's have dinner together!":
+        miHoyMeNoy = "I would love to!";
+        break;
+}
+return miHoyMeNoy;
+}
